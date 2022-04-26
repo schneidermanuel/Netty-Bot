79c9a63cbe6bf1691a63cf5eaa38be58822b6fe2
BrainyXS <brainyxs@gmail.com>
2022-04-26 17:37:38 +0200

    Added Caps Limit Rule

--- DiscordBot.Modules/AutoMod/AutoModModule.cs
+++ DiscordBot.Modules/AutoMod/AutoModModule.cs
@@ -3,6 +3,7 @@ using DiscordBot.Framework.Contract.Modularity;
 using DiscordBot.Framework.Contract.TimedAction;
 using DiscordBot.Modules.AutoMod.KeyValueValidationStrategies;
 using DiscordBot.Modules.AutoMod.Rules;
+using DiscordBot.Modules.AutoMod.Rules.Rules;
 
 namespace DiscordBot.Modules.AutoMod;
 
@@ -14,7 +15,9 @@ internal class AutoModModule : Module
         builder.RegisterType<AutoModCommands>().As<IGuildModule>();
         builder.RegisterType<AutoModBootStep>().As<ITimedAction>();
         builder.RegisterType<AutoModManager>().SingleInstance();
+
         builder.RegisterType<EmoteSpamAutoModRule>().As<IGuildAutoModRule>().SingleInstance();
+        builder.RegisterType<CapsLockAutoModRule>().As<IGuildAutoModRule>().SingleInstance();
 
         builder.RegisterType<BoolValueKeyValueValidationStrategy>().As<IKeyValueValidationStrategy>();
         builder.RegisterType<UnavailableKeyValueValidationStrategy>().As<IKeyValueValidationStrategy>();
---
+++ DiscordBot.Modules/AutoMod/Rules/Rules/CapsLockAutoModRule.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Discord.Commands;
+using DiscordBot.DataAccess.Contract.AutoMod;
+using DiscordBot.DataAccess.Contract.AutoMod.Violation;
+using DiscordBot.Framework.Extentions;
+
+namespace DiscordBot.Modules.AutoMod.Rules.Rules;
+
+internal class CapsLockAutoModRule : AutoModRuleBase
+{
+    public override string RuleIdentifier => "CAPS";
+
+    private const string CapsCountKey = "CAPS_LIMIT";
+    private const int DefaultCapsLimit = 3;
+
+    private const string WordsToIncrementLimitKey = "INCREMENT_COUNT";
+    private const int DefaultIncrementCount = 5;
+
+
+    public CapsLockAutoModRule(IAutoModBusinessLogic businessLogic) : base(businessLogic)
+    {
+    }
+
+    protected override Dictionary<string, ConfigurationValueType> _keys { get; } = new()
+    {
+        { ValidationHelper.ActionKey, ConfigurationValueType.ActionValue },
+        { CapsCountKey, ConfigurationValueType.IntValueOnly },
+        { WordsToIncrementLimitKey, ConfigurationValueType.IntValueOnly },
+    };
+
+
+    public override IRuleViolationAction ExecuteRule(ICommandContext context)
+    {
+        if (!_guilds.Contains(context.Guild.Id))
+        {
+            return new DoNothingAction();
+        }
+
+        var allowedCapsCount = Configs[context.Guild.Id].GetValue(CapsCountKey).ToInt()
+            .GetValueOrDefault(DefaultCapsLimit);
+        var words = context.Message.Content.Split(' ', '\n');
+        var capsCount = CalculateRelevantCapsCount(words);
+
+        var noCapsCount = words.Length - capsCount;
+        var wordsToIncrement = Configs[context.Guild.Id]
+            .GetValue(WordsToIncrementLimitKey).ToInt().GetValueOrDefault(DefaultIncrementCount);
+        var freeCapsCount = allowedCapsCount + noCapsCount / wordsToIncrement;
+        if (capsCount < freeCapsCount)
+        {
+            return new DoNothingAction();
+        }
+
+        var onlyEmotesAction =
+            ValidationHelper.MapValidation(Configs[context.Guild.Id].GetValue(ValidationHelper.ActionKey),
+                "Zu viele Caps!");
+        return onlyEmotesAction;
+    }
+
+    private int CalculateRelevantCapsCount(string[] words)
+    {
+        return words.Where(CountAsRelevantCaps).Count();
+    }
+
+    private bool CountAsRelevantCaps(string word)
+    {
+        if (word.Length < 3)
+        {
+            return false;
+        }
+
+        var charCount = word.Length;
+        var capsCount = word.Where(char.IsUpper).Count();
+
+        return capsCount > (charCount - capsCount) * 3;
+    }
+}
