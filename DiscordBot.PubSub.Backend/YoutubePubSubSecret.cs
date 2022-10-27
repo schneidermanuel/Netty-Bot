86fd3e1c6cde24a31668dc5a2898c7a48fe814b4
BrainyXS <brainyxs@gmail.com>
2022-10-27 17:14:24 +0200

    Encoding switching

--- DiscordBot.PubSub.Backend/YoutubePubSubSecret.cs
+++ DiscordBot.PubSub.Backend/YoutubePubSubSecret.cs
@@ -5,14 +5,14 @@ namespace DiscordBot.PubSub.Backend;
 
 public class YoutubePubSubSecret
 {
-    public static string Secret { get; } = Guid.NewGuid().ToString();
+    public static string Secret { get; } = "test";
 
     public static bool Check(string body, string signature)
     {
         using (var hmac = new HMACSHA1(Encoding.UTF8.GetBytes(Secret)))
         {
             var hashBytes = hmac.ComputeHash(Encoding.UTF8.GetBytes(body));
-            var hash = Encoding.UTF8.GetString(hashBytes);
+            var hash = BitConverter.ToString(hashBytes);
             Console.WriteLine("Computed Hash " + hash);
             return signature.Equals(hash);
         }
