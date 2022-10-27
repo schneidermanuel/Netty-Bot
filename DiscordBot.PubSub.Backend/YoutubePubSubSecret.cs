52b6e04a75088b65d152343fe46c97960015d084
BrainyXS <brainyxs@gmail.com>
2022-10-27 17:24:22 +0200

    Hash to lower Hex

--- DiscordBot.PubSub.Backend/YoutubePubSubSecret.cs
+++ DiscordBot.PubSub.Backend/YoutubePubSubSecret.cs
@@ -5,14 +5,27 @@ namespace DiscordBot.PubSub.Backend;
 
 public class YoutubePubSubSecret
 {
-    public static string Secret { get; } = "test";
+    private static string _secret;
+
+    public static string Secret
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(_secret))
+            {
+                _secret = new Guid().ToString();
+            }
+
+            return _secret;
+        }
+    }
 
     public static bool Check(string body, string signature)
     {
         using (var hmac = new HMACSHA1(Encoding.UTF8.GetBytes(Secret)))
         {
             var hashBytes = hmac.ComputeHash(Encoding.UTF8.GetBytes(body));
-            var hash = BitConverter.ToString(hashBytes);
+            var hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
             Console.WriteLine("Computed Hash " + hash);
             return signature.Equals(hash);
         }
