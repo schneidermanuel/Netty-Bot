d4f77bbea5f2c073c9f07c1028f49b6e0b7ffd82
BrainyXS <brainyxs@gmail.com>
2022-11-20 10:43:10 +0100

    Emoji APi

---
+++ DiscordBot.PubSub.Backend/Data/Guild/ReactionRole/Emoji.cs
@@ -0,0 +1,8 @@
+﻿namespace DiscordBot.PubSub.Backend.Data.Guild.ReactionRole;
+
+internal class Emoji
+{
+    public string Name { get; set; }
+    public string Url { get; set; }
+    public ulong Id { get; set; }
+}
